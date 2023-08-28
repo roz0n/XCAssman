a2bc73541373918f6275f137f81c518d2dd5ec7b
Arnold Rozon <arnold@rozon.org>
2023-08-27 19:07:27 -0700

    Initial commit

---
+++ XCAssman/XCAssmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAssmanApp.swift
+//  XCAssman
+//
+//  Created by Arnaldo Rozon on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct XCAssmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
